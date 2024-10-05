59ac6e0cb6a25d0f31ff87f14cd20f7696d30364
Cédric Duriau <duriau.cedric@live.be>
2024-10-05 20:54:00 +0200

    improve exporters; add tests; add pytest-freezegun dep

--- tests/unit/test_export/test_integrations/test_base.py
+++ tests/unit/test_export/test_integrations/test_base.py
@@ -1,4 +1,7 @@
 # stdlib
+import datetime
+import os
+import tempfile
 
 # third party
 import pytest
@@ -17,6 +20,19 @@ def test_init():
 def test_report_to_export_report(fix_report: Report):
     export_report = Exporter.report_to_export_report(fix_report)
     assert len(export_report.transactions) == 1
+
+
+def test_build_export_report_path(freezer, tmpdir, monkeypatch):
+    monkeypatch.setattr(tempfile, "gettempdir", lambda: str(tmpdir))
+    datetime_str = "1970-01-01T00:00:00.000Z"
+    freezer.move_to(datetime_str)
+    timestamp = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
+
+    exported = Exporter()
+    path = exported.build_export_report_path()
+    dirname, basename = os.path.split(path)
+    assert dirname == str(tmpdir)
+    assert basename == f"financebuddy_export_unknown_{timestamp}.unknown"
 
 
 def test_dump_report():
