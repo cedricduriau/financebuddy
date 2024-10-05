e2590c9117d8932078c435b405c55d95045cddb5
Cédric Duriau <duriau.cedric@live.be>
2024-10-05 20:56:09 +0200

    build path first for earler timestamp

--- financebuddy/export/integrations/financebuddy_csv.py
+++ financebuddy/export/integrations/financebuddy_csv.py
@@ -13,11 +13,10 @@ class FinanceBuddyCSVExporter(Exporter):
     extension: ExporterExtension = ExporterExtension.CSV
 
     def dump_report(self, export_report: ExportReport) -> str:
+        path = self.build_export_report_path()
         raw_report: dict = json.loads(export_report.model_dump_json())
         raw_transactions: list[dict[str, Any]] = raw_report.get("transactions", [dict()])
         headers = list(raw_transactions[0].keys())
-
-        path = self.build_export_report_path()
         with open(path, "w") as fp:
             writer = csv.DictWriter(fp, headers)
             writer.writeheader()
